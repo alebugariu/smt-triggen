4834673363786218404539e41c5d6e95cfe61a7f
bugariua <alexandra.bugariu@inf.ethz.ch>
2024-08-06 10:46:26 +0200

    updated the license

--- src/utils/toposort.py
+++ src/utils/toposort.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2021-2022 ETH Zurich.
+# Copyright (c) 2021-2024 ETH Zurich.
 ###############################################################################
 
 from functools import reduce
